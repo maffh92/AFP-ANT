627132f50717275efc6995ecb096d664043b2a82
martijn <martijn@mafl.me>
2017-03-28 15:16:34 +0200

    Added Enum instance for marker

--- src/Ant/Base.hs
+++ src/Ant/Base.hs
@@ -4,42 +4,65 @@
 module Ant.Base where
 
 {- describes the output state machine -}
-data TurnDir   = DLeft | DRight deriving (Show, Eq)
-data SenseDir  = Here | Ahead | LeftAhead | RightAhead deriving (Show, Eq)
-data Marker    = Zero | One | Two | Three | Four | Five deriving Eq
+data TurnDir
+  = DLeft
+  | DRight
+  deriving (Show, Eq)
 
-data Condition = Friend
-               | Foe
-               | FriendWithFood
-               | FoeWithFood
-               | Food
-               | Rock
-               | Marker Marker
-               | FoeMarker
-               | Home
-               | FoeHome
-               deriving (Show, Eq)
+data SenseDir
+  = Here
+  | Ahead
+  | LeftAhead
+  | RightAhead
+  deriving (Show, Eq)
+
+data Marker
+  = Zero
+  | One
+  | Two
+  | Three
+  | Four
+  | Five
+  deriving Eq
+
+instance Enum Marker where
+  fromEnum x = case x of
+    { Zero -> 0; One -> 1; Two -> 2; Three -> 3; Four -> 4; Five -> 5 }
+
+  toEnum x = case x `mod` 6 of
+    { 0 -> Zero; 1 -> One; 2 -> Two; 3 -> Three; 4 -> Four; 5 -> Five }
+
+instance Ord Marker where
+  x `compare` y = fromEnum x `compare` fromEnum y
+
+instance Show Marker where
+  show = show . fromEnum
+
+data Condition
+  = Friend
+  | Foe
+  | FriendWithFood
+  | FoeWithFood
+  | Food
+  | Rock
+  | Marker Marker
+  | FoeMarker
+  | Home
+  | FoeHome
+  deriving (Show, Eq)
 
 type State = Int
 
-data Command a = Sense SenseDir a a Condition
-               | Mark Marker a
-               | Unmark Marker a
-               | PickUp a a
-               | Drop a
-               | Turn TurnDir a
-               | Move a a
-               | Flip Int a a
-               deriving (Show, Eq, Functor, Foldable, Traversable)
-
-instance Show Marker where
-    show Zero   = "0"
-    show One    = "1"
-    show Two    = "2"
-    show Three  = "3"
-    show Four   = "4"
-    show Five   = "5"
-
+data Command a
+  = Sense SenseDir a a Condition
+  | Mark Marker a
+  | Unmark Marker a
+  | PickUp a a
+  | Drop a
+  | Turn TurnDir a
+  | Move a a
+  | Flip Int a a
+  deriving (Show, Eq, Functor, Foldable, Traversable)
 
 showCmds :: Show a => [(a, Command a)] -> String
 showCmds = unlines . map showInstr
