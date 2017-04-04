d16e5a29c5bf1049f0af239770758a3ffb3a6d9c
martijn <martijn@mafl.me>
2017-04-04 19:48:31 +0200

    asdf2

--- src/Genetic/Evolve.hs
+++ src/Genetic/Evolve.hs
@@ -5,6 +5,7 @@ module Genetic.Evolve
   , fitness
   ) where
 
+import Data.Array.IO
 import Ant.Base
 import Ant.Monad
 import Control.Lens
@@ -13,79 +14,92 @@ import Ant.Arbitrary.Base
 import Test.QuickCheck
 import Simulator
 import Simulator.Base
+import Genetic.ReadInstructions hiding (AntInstructions)
 
 data GeneticConfig = GeneticConfig
-  { _numRoundPerGeneration :: Int -- Simulate this many rounds per program
-  , _stopAfter             :: Int -- Stop search after this many generated programs
-  , _linesPerFile          :: Int -- Amount of lines per file
+  { _numRoundsPerGeneration :: Int -- Simulate this many rounds per program
+  , _stopAfter              :: Int -- Stop search after this many generated programs
+  , _linesPerFile           :: Int -- Amount of lines per file
   } deriving (Eq, Show)
 
 makeLenses ''GeneticConfig
 
 defaultGeneticConfig = GeneticConfig
-  { _numRoundPerGeneration = 10000
-  , _stopAfter             = 10000
-  , _linesPerFile          = 10000
+  { _numRoundsPerGeneration = 10000
+  , _stopAfter              = 1
+  , _linesPerFile           = 10000
   }
 
-generateProgram :: Int -> Gen AntMTest
-generateProgram n = do
-  let lbs = take n (iterate s z)
-  AntMTest <$> replicateM n (genCommand lbs)
-
-newProgram :: GeneticConfig -> IO [Command L]
-newProgram config = generate $ resize 1 $ newProgram' (view linesPerFile config)
-
--- | Generate a random program of n lines
-newProgram' :: Int -> Gen [Command L]
-newProgram' 0 = return []
-newProgram' n
-  | n > 0 = f n
-  | otherwise = fail "n is negative"
+-- | Generate a new program of length l
+newProgram :: Int -> IO [Command L]
+newProgram l = generate $ resize 1 $ newProgram' l
   where
-    f k = do
-      program <- generateProgram k
-      let program' = unAntMTest program
-      return program'
+    -- | Generate a random program of n lines
+    newProgram' 0 = return []
+    newProgram' n
+      | n > 0 = f n
+      | otherwise = fail "n is negative"
+      where
+        f k = do
+          program <- generateProgram k
+          let program' = unAntMTest program
+          return program'
+
+        generateProgram n = do
+          let lbs = take n (iterate s z)
+          AntMTest <$> replicateM n (genCommand lbs)
 
 type Fitness = Int
-type ProgramPair = ([Command L], Fitness)
 
 search :: GeneticConfig -> IO [Command L]
 search config =
   do -- Generate the initial pair of (program, fitness)
-    prog     <- newProgram config
+    prog      <- newProgram (view linesPerFile config)
+    worldF    <- readFile "Genetic/sample0.world"
+    blackProg <- readInstructions "Genetic/blackant.ant"
 
-    worldF <- readFile "...somefile"
-
-    _fitness <- fitness config worldF prog
+    _fitness <- fitness config worldF prog blackProg
     let e = (prog, _fitness)
 
     -- thunk of all the other programs to evaluate
-    programs <- replicateM (view stopAfter config - 1) (newProgram config)
+    programs <- replicateM (view stopAfter config - 1) (newProgram (view linesPerFile config))
+
 
     -- Evaluate all of them, keep the best one
-    (best, _) <- foldM (evalP config worldF) e programs
+    (best, _) <- foldM (\(_bp, _bf) p -> evalP config worldF _bf _bp blackProg p) e programs
     return best
 
 -- given a program and it's score, and another program, evaluate the score of
 -- the second program and compare it to the score of the first. The best one is
 -- kept and the other is discarded.
-evalP :: GeneticConfig -> String -> ProgramPair -> [Command L] -> IO ProgramPair
-evalP config worldF (prog1, _fitness1) prog2 =
+evalP
+  :: GeneticConfig   -- ^ configuration of the randomised search
+  -> String          -- ^ the world map, as a string
+  -> Fitness         -- ^ Fitness of the best program so far
+  -> [Command L]     -- ^ Current best program
+  -> AntInstructions -- ^ Default program to benchmark against, for the black colony
+  -> [Command L]     -- ^ The new program to test
+  -> IO ([Command L], Fitness)
+evalP config worldF _fitness1 prog1 blackProg prog2 =
   do
-    _fitness2 <- fitness config worldF prog2
+    _fitness2 <- fitness config worldF prog2 blackProg
+    putStrLn ("Comparing a program with score " ++ show _fitness1 ++ " to one that has fitness " ++ show _fitness2)
 
     return $ case _fitness1 `compare` _fitness2 of
       LT -> (prog2, _fitness2)
       EQ -> (prog1, _fitness1)
       GT -> (prog1, _fitness1)
 
-
 -- | Evaluate the program, use the final score as fitness
-fitness :: GeneticConfig -> String -> [Command L] -> IO Fitness
-fitness config worldF prog =
+fitness
+  :: GeneticConfig
+  -> String
+  -> [Command L]
+  -> AntInstructions
+  -> IO Fitness
+fitness config worldF prog blackProg  =
   do
-    gstate  <- initGameState 0 worldF prog
-    gstate' <- runNRounds (view numRoundPerGeneration config) gstate
-    return (redScore $ foodAdmin gstate')
+    gstate  <- initGameState 0 worldF prog blackProg
+    gstate' <- runNRounds (view numRoundsPerGeneration config) gstate
+    return . redScore $ foodAdmin gstate'
+
